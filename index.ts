d645a3bd4ac08f11762cd53548503a9c95d2d761
Tyler Gross <tylergross28@gmail.com>
2019-03-18 16:49:31 -0500

    working except bleeds are wrong

--- index.ts
+++ index.ts
@@ -1,1 +1,17 @@
-console.log('Hellow World!');
+const sourceFolder = Folder.selectDialog( 'Select the folder with Illustrator .ai files you want to convert to PDF');
+const files: any[] = sourceFolder.getFiles('*.ai');
+const destinationFolder = Folder.selectDialog( 'Select the to save the converted PDF files.', '~' );
+
+const saveOptions = new PDFSaveOptions();
+saveOptions.colorBars = true;
+saveOptions.trimMarks = true;
+saveOptions.pageInformation = true;
+saveOptions.registrationMarks = true;
+
+for (let i = 0; i < files.length; i++) {
+  const doc = app.open(files[i]);
+  const pdfFile = new File(`${destinationFolder}/${doc.name.replace(/.ai/, '')}.pdf`);
+  doc.saveAs(pdfFile, saveOptions);
+  doc.close();
+}
+alert(`Files are saved as PDF in ${destinationFolder}`);
