9bf45ec9cb70e21a3534ece757912cfadcab6b4d
Tyler Gross <tylergross28@gmail.com>
2019-03-18 17:54:08 -0500

    It works!

--- index.ts
+++ index.ts
@@ -2,16 +2,13 @@ const sourceFolder = Folder.selectDialog( 'Select the folder with Illustrator .a
 const files: any[] = sourceFolder.getFiles('*.ai');
 const destinationFolder = Folder.selectDialog( 'Select the to save the converted PDF files.', '~' );
 
-const saveOptions = new PDFSaveOptions();
-saveOptions.colorBars = true;
-saveOptions.trimMarks = true;
-saveOptions.pageInformation = true;
-saveOptions.registrationMarks = true;
+const saveOpts = new PDFSaveOptions();
+saveOpts.pDFPreset = 'ForScript';
 
 for (let i = 0; i < files.length; i++) {
   const doc = app.open(files[i]);
   const pdfFile = new File(`${destinationFolder}/${doc.name.replace(/.ai/, '')}.pdf`);
-  doc.saveAs(pdfFile, saveOptions);
+  doc.saveAs(pdfFile, saveOpts);
   doc.close();
 }
-alert(`Files are saved as PDF in ${destinationFolder}`);
+alert(`Success!`);
